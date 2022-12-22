7dfbca584291539bbbfe93721b4a0bb83dcf2c2c
CYee3589 <46963282+CYee3589@users.noreply.github.com>
2022-12-21 20:01:21 -0500

    Added Honor's Statement

--- BootlegWarioware/app/src/main/java/com/example/bootlegwarioware/MainActivity.java
+++ BootlegWarioware/app/src/main/java/com/example/bootlegwarioware/MainActivity.java
@@ -6,6 +6,22 @@ import android.os.Bundle;
 
 import java.util.ArrayList;
 
+/*
+Mobile App Development I -- COMP.4630 Honor Statement
+The practice of good ethical behavior is essential for maintaining good order in the classroom,
+providing an enriching learning experience for students, and training as a practicing computing
+professional upon graduation. This practice is manifested in the University's Academic Integrity
+policy. Students are expected to strictly avoid academic dishonesty and adhere to the Academic
+Integrity policy as outlined in the course catalog. Violations will be dealt with as outlined
+therein. All programming assignments in this class are to be done by the student alone unless
+otherwise specified. No outside help is permitted except the instructor and approved tutors.
+I certify that the work submitted with this assignment is mine and was generated in a manner
+consistent with this document, the course academic policy on the course website on Blackboard,
+and the UMass Lowell academic code.
+Date: 12/21/2022
+Name: Calvin Yee and Tony Choma
+*/
+
 public class MainActivity extends AppCompatActivity {
 
     private int livesLeft = 4;
